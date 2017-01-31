f69e30aa02fbdc214e0a1dd5a762cd90fb916abc
Jonathan Thom and Levi Bibo <jonathan@>
2017-01-31 08:11:39 -0800

    add blank api key file.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,12 @@
+export var Keys = {
+  tmdb: '',
+  guidebox: ''
+}
+
+export var masterFirebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}
