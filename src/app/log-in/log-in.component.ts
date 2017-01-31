dfff474e23d3e4ce5a1ec8e4b49ac10fd89965df
Erica Nafziger and Jonathan Thom <erica+jonathan@>
2017-01-30 16:11:12 -0800

    will create user object based on log in

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, AuthProviders } from 'angularfire2';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.css'],
+  providers: [UserService]
 })
 export class LogInComponent implements OnInit {
 user = {};
-  constructor(public af: AngularFire) {
+  constructor(public af: AngularFire, private us: UserService) {
     this.af.auth.subscribe(user => {
       if(user) {
         this.user = user;
-        console.log(this.user);
+        ///check to see if user already exists with service
+        // ;
       } else {
         //user not logged in
         this.user = {};
@@ -24,12 +27,12 @@ user = {};
   }
 
   login() {
-    this.af.auth.login({
-      provider: AuthProviders.Google
-    })
+    var loginReturn = this.us.login();
+    console.log('in log in' + this.user);
+    this.us.findOrMakeUser(this.user);
   }
 
   logout() {
-    this.af.auth.logout();
+    this.us.logout();
   }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { User } from './user.model';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable, AuthProviders } from 'angularfire2';
+
+@Injectable()
+export class UserService {
+  users: any = [];
+ constructor(private af: AngularFire) {
+   this.users = af.database.list('/users');
+ }
+
+ login() {
+  return this.af.auth.login({
+     provider: AuthProviders.Google
+   })
+ }
+
+ logout() {
+   return this.af.auth.logout();
+ }
+
+  findOrMakeUser(user: any) {
+    console.log(user);
+    debugger;
+    this.users.subscribe(users => {
+      for(var i = 0; i < users.length; i++) {
+        if(users[i].uid === user.google.uid) {
+          return;
+        }
+      }
+      var newUser = new User(user.google.uid, user.google.displayName, user.google.photoURL);
+      this.users.push(newUser);
+    })
+
+}
+}
+//   this.users.subscribe(users => {
+//   //
+// });
+//   for (var i = 0; i < users.length; i++) {
+//     if(users[i].uid === inputtedUser.uid) {
+//       return;
+//     }
+//   }
+//   var newUser = new User(inputtedUser.google.uid, inputtedUser.google.displayName, inputtedUser.google.photoURL);
+//   this.users.push(newUser);
+// });
